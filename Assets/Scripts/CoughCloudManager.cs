c048b5441838239e779c1f6f5e5e14e739230ba9
Bart van Dartel <LarsvanDartel73@gmail.com>
2020-11-23 15:22:38 +0100

    finished prototype for infection

--- Assets/Scripts/CoughCloudManager.cs
+++ Assets/Scripts/CoughCloudManager.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class CoughCloudManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace Profielwerkstuk { 
+    public class CoughCloudManager : MonoBehaviour
     {
-        
-    }
+        public float infectionRate;
+        private float sizeIncrease = 0.02f;
+        private List<PlayerMovement> playersInCloud;
+        void Start()
+        {
+            playersInCloud = new List<PlayerMovement>();
+            infectionRate = 1.0f;
+            StartCoroutine(destroyIn(5000));
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        void Update()
+        {
+            var newScale = transform.localScale;
+            newScale += new Vector3(sizeIncrease, sizeIncrease, sizeIncrease);
+            transform.localScale = newScale;
+
+            infectionRate -= Mathf.Pow(sizeIncrease, 3);
+
+            for(int i = playersInCloud.Count-1; i >= 0; i++)
+            {
+                var player = playersInCloud[i];
+                if (Random.Range(0.0f, 1.0f) < infectionRate)
+                {
+                    player.infect();
+                    playersInCloud.Remove(player);
+                }
+            }
+        }
+
+
+        IEnumerator destroyIn(int ms)
+        {
+            yield return new WaitForSeconds(ms / 1000);
+            Destroy(gameObject);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.transform.parent.name != "Players") return;
+            if (other.GetComponent<PlayerMovement>().infected) return;
+            if (!playersInCloud.Contains(other.gameObject.GetComponent<PlayerMovement>())) { playersInCloud.Add(other.GetComponent<PlayerMovement>()); }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.transform.parent.name != "Players") return;
+            if (other.GetComponent<PlayerMovement>().infected) return;
+            playersInCloud.Remove(other.GetComponent<PlayerMovement>());
+        }
+
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,8 +18,9 @@ namespace Profielwerkstuk
         private Vector3 target;
         public string status;
         public List<GameObject> waitingFor;
-        public GameObject CoughCloudPrefab;
-
+        public GameObject coughCloudPrefab;
+        public Material infectedMaterial;
+        public GameObject coughCloudParent;
         public bool infected = false;
 
         void Start()
@@ -29,6 +30,7 @@ namespace Profielwerkstuk
             {
                 Debug.Log(name + " is infected");
                 infected = true;
+                gameObject.GetComponent<MeshRenderer>().material = infectedMaterial;
             }
 
             obstacle.enabled = false;
@@ -38,22 +40,25 @@ namespace Profielwerkstuk
             agent.SetDestination(target);
             // targetIndicator.transform.position = target;
             // Debug.Log(name + " is now going to: " + target.x + ", " + target.y + ", " + target.z);
-            status = "ACTIVE";
+            status = "ACTIVE";  
             waitingFor = new List<GameObject>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            transform.GetComponent<Rigidbody>().velocity.Set(0, 0, 0);
             if (infected)
             {
-                if((Time.frameCount%1000) == 0)
+                if((Time.frameCount%500) == 0 && Random.Range(0.0f, 1.0f) > 0.5f)
                 {
-                    Debug.Log("Cough Cough");
+                    GameObject p = Instantiate(coughCloudPrefab, transform.position, transform.rotation);
+                    p.transform.GetComponent<Rigidbody>().velocity = transform.forward * 0.7f;
+                    p.transform.SetParent(coughCloudParent.transform);
                 }
             }
             // check if agent has reached goal
-            if (agent.enabled && !agent.isStopped && status != "SHOPPING")
+            if (agent.enabled && !agent.isStopped)
             {
                 var pos = transform.position;
                 if (Vector3.Distance(new Vector3(pos.x, target.y, pos.z), target) <= agent.stoppingDistance + gameObject.GetComponent<CapsuleCollider>().radius)
@@ -63,18 +68,20 @@ namespace Profielwerkstuk
                         taskManager.removeTask(target);
                         Debug.Log(name + " reached final destination");
                         StartCoroutine(activateObstacle());
+                        status = "DONE";
                     }
                     else
                     {
-                        Debug.Log(name + " reached target: " + (int)target.x + ", " + (int)target.z + ". Postition was: " + (int)pos.x + ", " + (int)pos.z);
+                        // Debug.Log(name + " reached target: " + (int)target.x + ", " + (int)target.z + ". Postition was: " + (int)pos.x + ", " + (int)pos.z);
                         StartCoroutine(waitForNextTask(Random.Range(500, 2000)));
                     }
                 }
             }
             if (status != "DONE" && taskManager.Tasks.Count == 0)
             {
-                status = "DONE";
+                Debug.Log(name + " reached final destination");
                 StartCoroutine(activateObstacle());
+                status = "DONE";
             }
             for (int i = waitingFor.Count - 1; i >= 0; i--)
             {
@@ -85,9 +92,9 @@ namespace Profielwerkstuk
                     waitingFor.Remove(player);
                     if (waitingFor.Count == 0)
                     {
-                        Debug.Log(name + " is no longer waiting.");
+                        // Debug.Log(name + " is no longer waiting.");
                         // Debug.Log(name + "'s state is now: ACTIVE");
-                        StartCoroutine(deactivateObstacle(false));
+                        StartCoroutine(deactivateObstacle());
                     }
                 }
                 else if (playerMovement.status == "SHOPPING")
@@ -96,9 +103,9 @@ namespace Profielwerkstuk
                     playerMovement.waitingFor.Add(gameObject);
                     if (waitingFor.Count == 0)
                     {
-                        Debug.Log(name + " is no longer waiting.");
+                        // Debug.Log(name + " is no longer waiting.");
                         // Debug.Log(name + "'s state is now: ACTIVE");
-                        StartCoroutine(deactivateObstacle(false));
+                        StartCoroutine(deactivateObstacle());
                     }
                 }
             }
@@ -110,12 +117,18 @@ namespace Profielwerkstuk
             if (other.transform.parent.name != "Players") return;
             // Debug.Log("Player " + other.gameObject.name + " just entered the collider");
             if (other.GetComponent<PlayerMovement>().status == "DONE") return;
+            if (other.GetComponent<PlayerMovement>().status == "SHOPPING") return;
             if (transform.GetSiblingIndex() < other.transform.GetSiblingIndex())
             {
+                if (waitingFor.Contains(other.gameObject)) return;
                 Debug.Log(name + " is now waiting for " + other.name);
                 waitingFor.Add(other.gameObject);
                 // Debug.Log(name + "'s state is now: INACTIVE");
                 StartCoroutine(activateObstacle());
+            }
+            else
+            {
+                target = taskManager.getTask(agent);
             }
         }
 
@@ -123,27 +136,25 @@ namespace Profielwerkstuk
         {
             if (other.transform.parent.name != "Players") return;
             if (status == "ACTIVE") return;
-            Debug.Log(name + ": " + other.gameObject.name + " just left the collider");
+            // Debug.Log(name + ": " + other.gameObject.name + " just left the collider");
             waitingFor.Remove(other.gameObject);
             if (waitingFor.Count == 0)
             {
                 Debug.Log(name + " is no longer waiting.");
                 // Debug.Log(name + "'s state is now: ACTIVE");
-                StartCoroutine(deactivateObstacle(false));
+                StartCoroutine(deactivateObstacle());
             }
         }
 
         IEnumerator waitForNextTask(float ms)
         {
             Debug.Log(name + " is waiting for " + ms + "ms");
-            status = "SHOPPING";
             StartCoroutine(activateObstacle());
+            status = "SHOPPING";
+            taskManager.removeTask(target);
             yield return new WaitForSeconds(ms / 1000);
             Debug.Log(name + " is done waiting.");
-            if (waitingFor.Count == 0)
-            {
-                StartCoroutine(deactivateObstacle(true));
-            }
+            if (waitingFor.Count == 0) { StartCoroutine(deactivateObstacle()); }
             else status = "INACTIVE";
         }
         IEnumerator activateObstacle()
@@ -154,21 +165,23 @@ namespace Profielwerkstuk
             status = "INACTIVE";
         }
 
-        IEnumerator deactivateObstacle(bool newTarget)
+        IEnumerator deactivateObstacle()
         {
             obstacle.enabled = false;
             yield return null;
             agent.enabled = true;
             yield return new WaitForEndOfFrame();
-            if (newTarget)
-            {
-                taskManager.removeTask(target);
-                target = taskManager.getTask(agent);
-            }
+            target = taskManager.getTask(agent);
             agent.SetDestination(target);
             yield return null;
             status = "ACTIVE";
         }
 
+        public void infect()
+        {
+            Debug.Log(name + " is infected");
+            infected = true;
+            gameObject.GetComponent<MeshRenderer>().material = infectedMaterial;
+        }
     }
 }
