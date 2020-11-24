caba944cee7327159d51056e83ac3bff8bd8e3de
Bart van Dartel <LarsvanDartel73@gmail.com>
2020-11-24 13:19:59 +0100

    reworked taskManager, implemented player flow

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,17 +10,16 @@ namespace Profielwerkstuk
     {
 
         public NavMeshAgent agent;
-        public GameObject ground;
         public int numTasks;
-        private TaskManager taskManager;
+        public TaskManager taskManager;
         public NavMeshObstacle obstacle;
         public CapsuleCollider triggerCollider;
-        private Vector3 target;
+        public Vector3 target;
         public string status;
         public List<GameObject> waitingFor;
         public GameObject coughCloudPrefab;
         public Material infectedMaterial;
-        public GameObject coughCloudParent;
+        public Transform coughCloudParent;
         public bool infected = false;
 
         private float timeSinceLastCough = 0.0f;
@@ -28,34 +27,26 @@ namespace Profielwerkstuk
 
         void Start()
         {
-            Time.timeScale = 10.0f;
-            if(Random.Range(0, 100) >= 50)
-            {
-                Debug.Log(name + " is infected");
-                infected = true;
-                gameObject.GetComponent<MeshRenderer>().material = infectedMaterial;
-            }
-
+            taskManager = new TaskManager(agent);
             timeUntilCough = Random.Range(10.0f, 30.0f);
-
             obstacle.enabled = false;
             agent.autoBraking = false;
-            taskManager = new TaskManager(ground, numTasks, agent);
-            target = taskManager.getTask(agent);
-            agent.SetDestination(target);
-            // targetIndicator.transform.position = target;
-            // Debug.Log(name + " is now going to: " + target.x + ", " + target.y + ", " + target.z);
-            status = "ACTIVE";  
+            status = "ASSIGNING";  
             waitingFor = new List<GameObject>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (status == "ASSIGNING") return;
+            if (status == "DONE")
+            {
+                Destroy(gameObject);
+                return;
+            }
+
             transform.GetComponent<Rigidbody>().velocity.Set(0, 0, 0);
             transform.GetComponent<Rigidbody>().rotation.Set(0, 0, 0, 0);
-
-            
 
             if (infected)
             {
@@ -65,9 +56,8 @@ namespace Profielwerkstuk
                 {
                     timeSinceLastCough = 0.0f;
                     timeUntilCough = Random.Range(10.0f, 30.0f);
-                    GameObject p = Instantiate(coughCloudPrefab, transform.position, transform.rotation);
+                    GameObject p = Instantiate(coughCloudPrefab, transform.position, transform.rotation, coughCloudParent);
                     p.transform.GetComponent<Rigidbody>().velocity = transform.forward * 0.7f;
-                    p.transform.SetParent(coughCloudParent.transform);
                 }
             }
             // check if agent has reached goal
@@ -79,82 +69,108 @@ namespace Profielwerkstuk
                     if (taskManager.Tasks.Count == 1)
                     {
                         taskManager.removeTask(target);
-                        Debug.Log(name + " reached final destination");
-                        StartCoroutine(activateObstacle());
-                        status = "DONE";
+                        Debug.Log(name + " reached final task");
+                        status = "CHECKING OUT";
+                        target = taskManager.registerPos;
+                        agent.SetDestination(target);
+                    }
+                    else if(taskManager.Tasks.Count == 0)
+                    {
+                        if (status == "CHECKING OUT")
+                        {
+                            print(name + " is at the register");
+                            status = "AT REGISTER";
+                            StartCoroutine(waitForNextTask(Random.Range(20000, 40000)));
+                        }
+                        if(status == "LEAVING")
+                        {
+                            print(name + " has left");
+                            status = "DONE";
+                        }
                     }
                     else
                     {
                         // Debug.Log(name + " reached target: " + (int)target.x + ", " + (int)target.z + ". Postition was: " + (int)pos.x + ", " + (int)pos.z);
+                        status = "SHOPPING";
+                        taskManager.removeTask(target);
                         StartCoroutine(waitForNextTask(Random.Range(500, 2000)));
                     }
                 }
             }
-            if (status != "DONE" && taskManager.Tasks.Count == 0)
+            if (agent.enabled)
             {
-                Debug.Log(name + " reached final destination");
-                StartCoroutine(activateObstacle());
-                status = "DONE";
-            }
-            for (int i = waitingFor.Count - 1; i >= 0; i--)
-            {
-                GameObject player = waitingFor[i];
-                PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
-                if (playerMovement.status == "DONE")
+                for (int i = waitingFor.Count - 1; i >= 0; i--)
                 {
-                    waitingFor.Remove(player);
-                    if (waitingFor.Count == 0)
+                    GameObject player = waitingFor[i];
+                    if (player == null)
                     {
-                        // Debug.Log(name + " is no longer waiting.");
-                        // Debug.Log(name + "'s state is now: ACTIVE");
-                        StartCoroutine(deactivateObstacle());
+                        waitingFor.Remove(player);
+                        continue;
+                    }
+                    PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+                    /* if (playerMovement.status == "DONE")
+                    {
+                        waitingFor.Remove(player);
+                        if (waitingFor.Count == 0)
+                        {
+                            // Debug.Log(name + " is no longer waiting.");
+                            // Debug.Log(name + "'s state is now: ACTIVE");
+                            StartCoroutine(deactivateObstacle());
+                        }
+                    }
+                    else*/
+                    if (playerMovement.status == "SHOPPING" || playerMovement.status == "AT REGISTER")
+                    {
+                        waitingFor.Remove(player);
+                        playerMovement.waitingFor.Add(gameObject);
+                        if (waitingFor.Count == 0)
+                        {
+                            // Debug.Log(name + " is no longer waiting.");
+                            // Debug.Log(name + "'s state is now: ACTIVE");
+                            StartCoroutine(deactivateObstacle());
+                        }
                     }
                 }
-                else if (playerMovement.status == "SHOPPING")
-                {
-                    waitingFor.Remove(player);
-                    playerMovement.waitingFor.Add(gameObject);
-                    if (waitingFor.Count == 0)
-                    {
-                        // Debug.Log(name + " is no longer waiting.");
-                        // Debug.Log(name + "'s state is now: ACTIVE");
-                        StartCoroutine(deactivateObstacle());
-                    }
-                }
             }
-
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.transform.parent.name != "Players") return;
             // Debug.Log("Player " + other.gameObject.name + " just entered the collider");
-            if (other.GetComponent<PlayerMovement>().status == "DONE") return;
+            // if (other.GetComponent<PlayerMovement>().status == "DONE") return;
             if (other.GetComponent<PlayerMovement>().status == "SHOPPING") return;
+            if (other.GetComponent<PlayerMovement>().status == "AT REGISTER") return;
             if (transform.GetSiblingIndex() < other.transform.GetSiblingIndex())
             {
                 if (waitingFor.Contains(other.gameObject)) return;
-                Debug.Log(name + " is now waiting for " + other.name);
+                // Debug.Log(name + " is now waiting for " + other.name);
                 waitingFor.Add(other.gameObject);
                 // Debug.Log(name + "'s state is now: INACTIVE");
                 StartCoroutine(activateObstacle());
-                status = "INACTIVE";
+                // status = "INACTIVE";
             }
             else
             {
-                if(status == "ACTIVE") target = taskManager.getTask(agent);
+                if (agent.enabled)
+                {
+                    if (status == "ACTIVE")
+                    {
+                        target = taskManager.getTask();
+                        agent.SetDestination(target);
+                    }
+                }                    
             }
         }
 
         private void OnTriggerExit(Collider other)
         {
             if (other.transform.parent.name != "Players") return;
-            if (status == "ACTIVE") return;
             // Debug.Log(name + ": " + other.gameObject.name + " just left the collider");
             waitingFor.Remove(other.gameObject);
             if (waitingFor.Count == 0)
             {
-                Debug.Log(name + " is no longer waiting.");
+                // Debug.Log(name + " is no longer waiting.");
                 // Debug.Log(name + "'s state is now: ACTIVE");
                 StartCoroutine(deactivateObstacle());
             }
@@ -162,20 +178,18 @@ namespace Profielwerkstuk
 
         IEnumerator waitForNextTask(float ms)
         {
-            Debug.Log(name + " is waiting for " + ms + "ms");
+            // Debug.Log(name + " is waiting for " + ms + "ms");
             StartCoroutine(activateObstacle());
-            status = "SHOPPING";
-            taskManager.removeTask(target);
             yield return new WaitForSeconds(ms / 1000);
-            Debug.Log(name + " is done waiting.");
+            // print(name + " is done waiting.");
             if (waitingFor.Count == 0) { StartCoroutine(deactivateObstacle()); }
-            else status = "INACTIVE";
         }
         IEnumerator activateObstacle()
         {
             agent.enabled = false;
-            yield return null;
+            yield return null;  
             obstacle.enabled = true;
+            yield return null;
         }
 
         IEnumerator deactivateObstacle()
@@ -184,10 +198,20 @@ namespace Profielwerkstuk
             yield return null;
             agent.enabled = true;
             yield return new WaitForEndOfFrame();
-            target = taskManager.getTask(agent);
+            if (taskManager.Tasks.Count == 0)
+            {
+                if (status == "CHECKING OUT")
+                {
+                    target = taskManager.registerPos;
+                }
+                if (status == "AT REGISTER" || status == "LEAVING")
+                {
+                    target = taskManager.leavingPos;
+                    status = "LEAVING";
+                }
+            }
+            else target = taskManager.getTask();
             agent.SetDestination(target);
-            yield return null;
-            status = "ACTIVE";
         }
 
         public void infect()
@@ -198,3 +222,4 @@ namespace Profielwerkstuk
         }
     }
 }
+
---
+++ Assets/Scripts/flowManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace Profielwerkstuk
+{
+    public class flowManager : MonoBehaviour
+    {
+
+        // private float time = 0.0f;
+
+        /* When spawning a new player, the following variables need to be assigned:
+         * Position
+         * Ground
+         * Number of tasks
+         * Parent of coughclouds
+        */
+
+        public Transform spawningGround;
+        public Transform taskGround;
+        public Transform registerGround;
+        public Transform leavingGround;
+    
+        public Transform ground;
+        public Transform coughClouds;
+
+        public GameObject playerPrefab;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            Time.timeScale = 10.0f;
+            StartCoroutine(spawnPlayers(5));
+        }
+
+        IEnumerator spawnPlayers(int numPlayers)
+        {
+
+            var minX = spawningGround.position.x - spawningGround.localScale.x / 2;
+            var maxX = spawningGround.position.x + spawningGround.localScale.x / 2;
+            var minZ = spawningGround.position.z - spawningGround.localScale.z / 2;
+            var maxZ = spawningGround.position.z + spawningGround.localScale.z / 2;
+            var y = spawningGround.position.y + spawningGround.localScale.y / 2;
+
+
+            for (int i = 0; i < numPlayers; i++)
+            {
+                // print("Spawning player " + (i + 1));
+            
+                // Generates starting position
+                float x = Random.Range(minX, maxX);
+                float z = Random.Range(minZ, maxZ);
+                var spawnPosition = new Vector3(x, y, z);
+
+                // Spawns player    
+                GameObject player = Instantiate(playerPrefab, spawnPosition, Quaternion.identity, transform);
+                yield return new WaitForEndOfFrame();
+                player.name = "" + (i + 1);
+                PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+                playerMovement.coughCloudParent = coughClouds;
+
+                // Infects player
+                if (Random.Range(0, 100) >= 50)
+                {
+                    playerMovement.infected = true;
+                    player.GetComponent<MeshRenderer>().material = playerMovement.infectedMaterial;
+                }
+                // Assigns Tasks
+                TaskManager taskManager = playerMovement.taskManager;
+                NavMeshAgent agent = playerMovement.agent;
+                int numTasks = Random.Range(5, 10);
+                for(int t = 0; t < numTasks; t++)
+                {
+                    taskManager.addTaskInArea(taskGround);
+                }
+                taskManager.addPos("register", registerGround);
+                taskManager.addPos("leaving", leavingGround);
+
+                playerMovement.target = taskManager.getTask();
+                agent.SetDestination(playerMovement.target);
+
+                playerMovement.status = "ACTIVE";
+                yield return new WaitForSeconds(10);
+            }
+        }
+
+    }
+}
