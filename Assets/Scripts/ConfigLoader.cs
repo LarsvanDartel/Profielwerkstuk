efddbd2f1d1821f923bf8d0c147a153cae3b3b02
SuperVK <victor.klomp@gmail.com>
2020-11-24 13:37:00 +0100

    added global static class

--- Assets/Scripts/ConfigLoader.cs
+++ Assets/Scripts/ConfigLoader.cs
@@ -6,14 +6,19 @@ namespace Profielwerkstuk {
     public class ConfigLoader : MonoBehaviour
     {
         public TextAsset JSONFile;
-        private EmptyConfig config;
+        public EmptyConfig config;
         // Start is called before the first frame update
         void Start()
         {
             config = JsonUtility.FromJson<EmptyConfig>(JSONFile.ToString());
 
-            print(EmptyConfig.speed);
-            // Time.timeScale = config.speed;
+            Config.speed = config.speed;
+            Config.coughCloudIncrease = config.coughCloudIncrease;
+            Config.infectionRateCC = config.infectionRateCC; 
+            Config.minCough = config.infectionRateCC; 
+            Config.maxCough = config.maxCough; 
+            Config.chanceInfected = config.chanceInfected;
+            Config.spawnsPerHour = config.spawnsPerHour; 
         }
 
         // Update is called once per frame
--- Assets/Scripts/EmptyConfig.cs
+++ Assets/Scripts/EmptyConfig.cs
@@ -5,6 +5,16 @@ namespace Profielwerkstuk {
     [System.Serializable]
     public class EmptyConfig
     {
+        public float speed; // the speed of the simulation
+        public float coughCloudIncrease; // the rate at which the cough cloud increases and the chance decreases
+        public float infectionRateCC; // the starting chance you get infected from a cough cloud
+        public float minCough; // the minimal time between each cough
+        public float maxCough; // the maximal time between each cough
+        public float chanceInfected; // a chance 0-10 someone spawns infected
+        public float spawnsPerHour; // how many people spawn per hour       
+
+    }
+    public static class Config {
         public static float speed; // the speed of the simulation
         public static float coughCloudIncrease; // the rate at which the cough cloud increases and the chance decreases
         public static float infectionRateCC; // the starting chance you get infected from a cough cloud
