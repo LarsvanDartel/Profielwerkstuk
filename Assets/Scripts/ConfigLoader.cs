a76095e247b1b5575b8d12e59e4617b65c9ff0cb
SuperVK <victor.klomp@gmail.com>
2020-11-24 11:04:36 +0100

    added config

---
+++ Assets/Scripts/ConfigLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Profielwerkstuk { 
+    public class ConfigLoader : MonoBehaviour
+    {
+        public TextAsset JSONFile;
+        private EmptyConfig config;
+
+        public PlayerMovement playerMovement;
+        // Start is called before the first frame update
+        void Start()
+        {
+            config = JsonUtility.FromJson<EmptyConfig>(JSONFile.ToString());
+
+            print(EmptyConfig.speed);
+            // Time.timeScale = config.speed;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
