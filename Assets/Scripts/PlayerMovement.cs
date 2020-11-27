8acaa66761f98938457da9aeb7234d2748745446
Bart van Dartel <LarsvanDartel73@gmail.com>
2020-11-27 12:55:09 +0100

    minor changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -75,7 +75,11 @@ namespace Profielwerkstuk
                 {   
                     if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
                     {
-                        if (Vector3.Distance(new Vector3(pos.x, target.y, pos.y), target) < agent.stoppingDistance + agent.radius) print(name + " is sus");
+                        if (Vector3.Distance(new Vector3(pos.x, target.y, pos.y), target) < agent.stoppingDistance + agent.radius + 1)
+                        {
+                            Utility.PrintVector(pos);
+                            Utility.PrintVector(target);
+                        }
                         if (status == "ACTIVE") taskManager.RemoveTask(target);
                         if (taskManager.Tasks.Count == 0)
                         {
